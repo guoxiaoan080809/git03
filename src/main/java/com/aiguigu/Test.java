9d9fb56fe1fb1261aba959735e8b93688195b078
an <1820646086@qq.com>
2021-11-03 18:19:03 +0800

    Initial commit

---
+++ src/main/java/com/aiguigu/Test.java
@@ -0,0 +1,14 @@
+package com.aiguigu;
+
+/**
+ * @ClassName Test
+ * @Description
+ * @Author An
+ * @Date 2021/11/3 18:18
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
